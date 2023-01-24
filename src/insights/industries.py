0d54cc97bba01ba243694dfe064ebe91aebcc415
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-01-24 19:45:35 -0300

    Implementa a função filter_by_industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -11,19 +11,18 @@ def get_unique_industries(path: str) -> List[str]:
     return industries
 
 
+# print(get_unique_industries("data/jobs.csv"))
+
+
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
+    filtered_jobs_by_industry = []
 
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    for job in jobs:
+        if industry == job["industry"]:
+            filtered_jobs_by_industry.append(job)
+
+    return filtered_jobs_by_industry
+
+
+# print(filter_by_industry(read("data/jobs.csv"), "Finance"))
