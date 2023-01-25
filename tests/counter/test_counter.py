be9f9cbc029569519f7cf341f5ee3344cad87354
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-01-25 18:18:04 -0300

    Implementa teste count_ocurrences e read_brazillian_file

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,6 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("data/jobs.csv", "analyst") == 1100
+    assert count_ocurrences("data/jobs.csv", "engineer") == 5017
